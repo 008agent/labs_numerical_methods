9a5be9d14190c0bb57c80d94c6ee3dca6e2f7ce7
vratnikov <008agent@mail.ru>
2013-06-22 23:00:09 +0400

    added bulk to gauss numerical method lab

---
+++ lab1_gauss/lab1_gauss/Printer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab1_gauss
+{
+    class Printer
+    {
+        /// <summary>
+        /// Выводит матрицу
+        /// </summary>
+        /// <param name="src">Принимает исходную матрицу</param>
+        public static void print_2d_array(double[,] src)
+        {
+            int szRow = src.GetLength(0);
+            int szCol = src.GetLength(1);
+           // Console.WriteLine(szRow);
+           // Console.WriteLine(szCol);
+
+            for (int i = 0; i < szRow; i++)
+            {
+                for (int j = 0; j < szCol; j++)
+                {
+                    Console.Write( " <" + Convert.ToString(src[i, j]) + "> ");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ lab1_gauss/lab1_gauss/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab1_gauss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gauss g = new Gauss(3);
+            g.init_matrix();
+            Printer.print_2d_array(g.getMatrix);
+            g.init_matrix_randoms(1, 10);
+            Printer.print_2d_array(g.getMatrix);
+            Console.ReadKey();
+        }
+    }
+}
